1e04da0f2b34e0d8249cdd698c274dd5d195eb5a
MA0610 <47308923+MA0610@users.noreply.github.com>
2022-02-28 17:25:58 -0800

    Add files via upload

---
+++ robot/commands/resetTicksCommand.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+
+public class resetTicksCommand extends CommandBase {
+  /** Creates a new resetTicksCommand. */
+  public resetTicksCommand() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(RobotContainer.mEncoderSub);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    RobotContainer.mEncoderSub.resetTicks();
+
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    //RobotContainer.mEncoderSub.resetTicks();
+
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return RobotContainer.mEncoderSub.resetTicksCheck();
+  }
+}
